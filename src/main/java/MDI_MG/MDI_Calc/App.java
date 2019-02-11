7f1a98f3e79117864e8d3c18f914be46ccb974bc
agegout <adrien.gegout@gmail.com>
2019-02-11 15:08:35 +0100

    initilisation2

---
+++ src/main/java/MDI_MG/MDI_Calc/App.java
@@ -0,0 +1,13 @@
+package MDI_MG.MDI_Calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

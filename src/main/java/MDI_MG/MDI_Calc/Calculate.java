774d1c2ee297601219f2c6b4aa8c9adc86069ff2
Moisan Simon <simon.moisan@etudiant.univ-rennes1.fr>
2019-02-11 17:18:35 +0100

    <Mise à jour de la fonction normale> This commit does the following: * Implémentation du choix entre 4 fonctions (add,sub,mult et div) et renvoie du résultat associé

--- src/main/java/MDI_MG/MDI_Calc/Calculate.java
+++ src/main/java/MDI_MG/MDI_Calc/Calculate.java
@@ -5,19 +5,21 @@ import MDI_MG.MDI_Calc.Normal;
 import MDI_MG.MDI_Calc.Scientifique;
 
 public class Calculate {
-    private boolean isOn = false;
-    private boolean mode = false; // false = normale et true = scientifique
-    private double prec = 0.0;
-    private int answer = 0;
+
 
     //On choisi le mode de la calculatrice
-    void main()
+    public static void main( String[] args )
     {
+         boolean isOn = false;
+         boolean mode = false; // false = normale et true = scientifique
+         double prec = 0.0;
+         int answer = 0;
+
         System.out.println("Bonjour, bienvenue sur la calcuatrice");
         isOn = true;
         Scanner scan = new Scanner(System.in);
 
-        ChooseCalculateMode(scan);
+        ChooseCalculateMode(scan, mode);
         Normal nor = new Normal();
         Scientifique sci = new Scientifique();
 
@@ -31,20 +33,20 @@ public class Calculate {
             }
             else {
                 System.out.println("Mode Scientifique");
-                prec = sci.init(prec,scan);
+                //prec = sci.init(prec,scan);
             }
 
             System.out.println("Le résultat est : "+prec);
         }
     }
 
-    public void ChooseCalculateMode(Scanner scan)
+    public static void ChooseCalculateMode(Scanner scan, boolean mode)
     {
         System.out.println("Rentrez le mode de la calculatrice que vous voulez utiliser : ");
         System.out.println("Appuyez sur 1 : Mode normale");
         System.out.println("Appuyez sur 2 : Mode scientifique");
 
-        answer = scan.nextInt();
+        int answer = scan.nextInt();
 
         if(answer == 1)
         {
